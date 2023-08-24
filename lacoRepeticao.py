e0cf3c6bd18e30e9b9aeb3cdbe3e87da50683a81
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2023-08-24 20:05:56 -0300

    conceito inicial de laco de repeticao

---
+++ lacoRepeticao.py
@@ -0,0 +1,26 @@
+#   LACO DE REPETICAO
+i = 1
+while i <= 10:
+#   print(i)
+  i += 1
+
+#   LACO DE REPETICAO QUANDO CHEGA AO NUMERO 5
+i = 1
+while i <= 10:
+#   print(i)
+  if i == 5: # para quando chega em 5
+    break
+  i += 1
+
+#   LACO DE REPETICAO QUANDO CHEGA AO NUMERO 5 ELE PULA E CONTINUA NO 6
+i = 1
+while i <= 10:
+  i += 1
+  if i == 5: # pula o  5
+    continue
+#   print(i)
+
+#   LACO DE REPETICAO LER A LISTA
+l = [1, 2, 3, 4, 5]
+for x in l:
+#   print(x)
