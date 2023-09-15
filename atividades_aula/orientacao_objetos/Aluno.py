a66d32090e208625b383c7735160ed62a1f1c0d8
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2023-09-14 21:36:17 -0300

    hierarquia de classes

---
+++ atividades_aula/orientacao_objetos/Aluno.py
@@ -0,0 +1,16 @@
+#   heranca
+class Aluno(Pessoa):
+    def __init__(self, nome, idade, matricula):
+        # metodo construtor de Pessoas 
+        super().__init__(nome, idade)
+
+    def getMatricula(self):
+        return self.matricula
+    
+#   cria um objeto da classe aluno
+a = Aluno("Joao", 30, 9999)
+
+#   chama os metodos da classe aluno
+print(a.getNome())
+print(a.getIdade())
+print(a.getMatricula())
