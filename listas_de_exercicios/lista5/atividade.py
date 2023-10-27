2425ef2c34fd23fb4a87c8eee7945bec9fd1df2b
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2023-10-26 21:33:17 -0300

    iniciacao da lista de exercicios 5 coneccao com banco e inicio de insert

---
+++ listas_de_exercicios/lista5/atividade.py
@@ -0,0 +1,32 @@
+import connectBanco
+
+con = connectBanco.getConexao()
+cursor = con.cursor()
+
+# sql = "select * from item_venda"
+# cursor.execute(sql)
+
+# resultado = cursor.fetchall()
+
+# for x in resultado:
+#   print(x)
+
+# con.close
+
+#   Escreva um programa que utilize o banco de dados estoque para inserir itens em uma venda e abater do estoque.
+sql = "INSERT INTO venda (id_venda, id_vendedor, id_cliente, vl_total_venda) VALUES (default, %s, %s)"
+par = ()
+cursor.execute(sql, par)
+
+con.commit()
+
+print(cursor.rowcount, "registro inserido")
+print("id_cliente:", cursor.lastrowid)
+
+con.close
+
+#   Escreva um programa que utilize o banco de dados estoque para exibir todas as vendas de um vendedor (nome) em um determinado mês e ano.
+
+
+#   Escreva um programa que utilize o banco de dados estoque para exportar os dados de todas as vendas para um documento json.
+
