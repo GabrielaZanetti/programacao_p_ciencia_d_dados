f567534ff0382ce1a470923e3d0348d90f4df34c
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2023-10-26 21:00:43 -0300

    coneccao/insert/update/delete de dados

--- atividades_aula/sql/banco.py
+++ atividades_aula/sql/banco.py
@@ -1,12 +1,10 @@
 import mysql.connector
 
 def getConexao():
-    con = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "user",
-        database = "estoque"
-    )
-    return con
-
-print(getConexao)
+  con = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="estoque"
+  )
+  return con
--- atividades_aula/sql/insert_prepared.py
+++ atividades_aula/sql/insert_prepared.py
@@ -2,9 +2,9 @@ import mysql.connector
 import banco
 
 con = banco.getConexao()
-cursor = con.cursor()
+cursor = con.cursor(prepared=True)
 
-sql = "INSERT INTO cliente (id_cliente, nome_cliente, id_tipo_cliente) VALUES (default, %s, %s)"
+sql = "INSERT INTO cliente (id_cliente, nome_cliente, id_tipo_cliente) VALUES (default, ?, ?)"
 par = ("Dionatan", 5)
 cursor.execute(sql, par)
 
